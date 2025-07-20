cd514de7377e42ff6dcc4e2c3b479e7b8b574937
MuhShahram <mshahram574@gmail.com>
2025-07-21 01:13:02 +0500

    add header section and side bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/*html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+      fontFamily: {
+        roboto: "Roboto, sans-serif",
+      },
+      boxShadow: {
+        "custom-Black": "0 2px 7px rgba(0, 0, 0, 0.22)",
+      },
+      minHeight: {
+        "custom-full": "89vh",
+      },
+    },
   },
   plugins: [],
 };
