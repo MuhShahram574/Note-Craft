e3e2ef656914cf1198043b4af73b71e36eb840fa
MuhShahram <mshahram574@gmail.com>
2025-07-21 15:46:01 +0500

    add new node functionality

--- public/script.js
+++ public/script.js
@@ -1,19 +1,23 @@
 const radioBtns = document.querySelectorAll(".radio-btn");
 const addNotesSec = document.getElementById("add-notes-section");
-const notesFormBtns = document.querySelectorAll("#add-notes-btn");
-console.log(notesFormBtns);
+const notesFormBtns = document.querySelectorAll(".add-notes-btn");
+const newNoteBtn = document.getElementById("new-note");
+const newNoteTitle = document.getElementById("title");
+const newNoteDiscription = document.getElementById("discription");
+
+const clearFields = function (field_1, field_2) {
+  field_1.value = field_2.value = "";
+};
+//
+const removeClass = (btn) =>
+  btn.classList.remove("bg-yellow-600", "bg-green-600", "bg-purple-600");
 
 function radioBtn() {
   radioBtns.forEach((btn) => {
+    removeClass(btn);
     btn.addEventListener("click", () => {
       const btnColor = btn.nextElementSibling.textContent.toLowerCase();
-
-      // First, remove existing color classes
-      radioBtns.forEach((btn) =>
-        btn.classList.remove("bg-yellow-600", "bg-green-600", "bg-purple-600")
-      );
-
-      // Then add the correct one
+      radioBtns.forEach((btn) => removeClass(btn));
       if (["purple", "green", "yellow"].includes(btnColor)) {
         btn.classList.add(`bg-${btnColor}-600`);
       }
@@ -22,4 +26,20 @@ function radioBtn() {
 }
 radioBtn();
 
-function addNotedisAppear() {}
+function addNoteDisappear() {
+  notesFormBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      addNotesSec.classList.add("opacity-0");
+      addNotesSec.classList.add("hidden");
+      addNotesSec.classList.remove("flex");
+      radioBtns.forEach((btn) => removeClass(btn));
+      clearFields(newNoteTitle, newNoteDiscription);
+    });
+  });
+  newNoteBtn.addEventListener("click", () => {
+    addNotesSec.classList.remove("hidden");
+    addNotesSec.classList.add("flex");
+    addNotesSec.classList.remove("opacity-0");
+  });
+}
+addNoteDisappear();
