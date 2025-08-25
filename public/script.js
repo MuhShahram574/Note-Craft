588ba7fd69d5e81d0fef73f81f683e827ebdf878
Muhammad Shahram <mshahram574@gmail.com>
2025-08-25 22:19:14 +0500

    Full task show feature added

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@
 // Sections
 const addNotesSec = document.getElementById("add-notes-section");
 const notesContainer = document.getElementById("notes-container");
+const showNoteSec = document.getElementById("show-full-note");
 
 // Elements
 const newNoteBtn = document.getElementById("new-note");
@@ -17,6 +18,7 @@ const aside = document.querySelector("aside");
 const newNoteTitle = document.getElementById("title");
 const newNoteDiscription = document.getElementById("description");
 searchBar = document.querySelector(".search-bar");
+const noteShowBox = document.querySelector(".note-show-box");
 
 // Buttons
 const radioBtns = [...document.querySelectorAll(".radio-btn")]; // NodeList -> Array
@@ -32,6 +34,7 @@ const reminderContainerBtn = document.querySelector(".reminder");
 // CONSTANTS
 // ------------------- //
 const COLOR_CLASSES_600 = ["bg-blue-600", "bg-green-600", "bg-red-600"];
+const COLOR_CLASSES_500 = ["bg-blue-500", "bg-green-500", "bg-red-500"];
 const COLOR_KEYS = ["blue", "green", "red"]; // supported colors
 
 // ------------------- //
@@ -137,14 +140,14 @@ const createNewNote = function (arr, showRestoreBtn = false) {
     const html = `
       <div
         id="${item.id}"
-        class="task-box shadow-custom-Black w-full break-inside-avoid rounded-lg p-2.5 md:p-5 flex flex-col justify-start gap-3 ${bgColor} relative group"
+        class="task-box shadow-custom-Black w-full break-inside-avoid rounded-lg p-2.5 md:p-5 flex flex-col justify-start gap-3 ${bgColor} relative group cursor-context-menu"
       >
-        <div class="delete-btn flex justify-center items-center bg-red-600 hover:bg-red-700 w-fit py-1 px-1.5 rounded-lg transition-all duration-200 shadow-custom-Black -translate-y-1 active:translate-y-0 active:shadow-none cursor-pointer absolute right-0 top-0 opacity-0 group-hover:opacity-100">
+        <div class="btn delete-btn flex justify-center items-center bg-red-600 hover:bg-red-700 w-fit py-1 px-1.5 rounded-lg transition-all duration-200 shadow-custom-Black -translate-y-1 active:translate-y-0 active:shadow-none cursor-pointer absolute right-0 top-0 opacity-0 group-hover:opacity-100">
           <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="16" viewBox="0 0 384 512">
             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
           </svg>
         </div>
-        <div class="restore-btn ${
+        <div class="btn restore-btn ${
           showRestoreBtn ? "" : "hidden"
         } justify-center items-center bg-green-600 hover:bg-green-700 w-fit py-1 px-1.5 rounded-lg transition-all duration-200 shadow-custom-Black -translate-y-1 active:translate-y-0 active:shadow-none cursor-pointer absolute right-7 top-0 opacity-0 group-hover:opacity-100">
           <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="16" viewBox="0 0 640 640">
@@ -338,6 +341,48 @@ const fixClass = function (e) {
   e.currentTarget.classList.add("bg-purple-600/30");
 };
 
+// ------------------- //
+// Full Task View
+// ------------------- //
+const showTask = function () {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !showNoteSec.classList.contains("hidden")) {
+      showNoteSec.classList.add("hidden");
+    }
+  });
+  document.addEventListener("click", function (e) {
+    if (
+      !e.target.closest(".task-box") &&
+      !showNoteSec.classList.contains("hidden") &&
+      e.target.closest(".note-show-box") !== noteShowBox
+    ) {
+      showNoteSec.classList.add("hidden");
+    }
+  });
+
+  const viewTask = function (e) {
+    const taskBox = e.target.closest(".task-box");
+    const taskId = +taskBox.id;
+    if (taskBox && !e.target.closest(".btn")) {
+      const arr = curContainer === "notes" ? tasks : deletedTasks;
+      const obj = arr.find((t) => t.id === taskId);
+      const title = showNoteSec.querySelector(".full-note-title");
+      const description = showNoteSec.querySelector(".full-note-description");
+      const noteBox = showNoteSec.querySelector("div");
+      showNoteSec.classList.add("hidden");
+      title.textContent = obj.title;
+      description.textContent = obj.description;
+      removeClass(noteBox, COLOR_CLASSES_500);
+      noteBox.classList.add(`bg-${obj.bgColor}-500`);
+      toggleClasses(showNoteSec, ["flex"], ["hidden"]);
+    }
+  };
+
+  notesContainer.addEventListener("click", (e) => {
+    viewTask(e);
+  });
+};
+
 function appperNotes() {
   if (!notesContainerBtn) return;
   notesContainerBtn.addEventListener("click", (e) => {
@@ -399,5 +444,6 @@ function init() {
   appperDeletedNotes();
   appperAchievedNotes();
   appperReminderNotes();
+  showTask();
 }
 init();
--- public/theme.js
+++ public/theme.js
@@ -4,7 +4,6 @@ let searchBar = document.querySelector(".search-bar");
 const lightIcon = document.querySelector(".light");
 const darkIcon = document.querySelector(".dark");
 
-
 // Dark Theme
 function darkTheme() {
   toggleClasses(
@@ -28,7 +27,7 @@ function lightTheme() {
     ["bg-dark", "text-gray-100"]
   );
 
-  toggleClasses(addNotesSec, ["bg-white"], ["bg-dark"]);
+  toggleClasses(addNotesSec, ["bg-gray-100"], ["bg-dark"]);
 
   toggleClasses(searchBar, ["text-gray-600"], ["text-gray-300"]);
 
@@ -37,19 +36,18 @@ function lightTheme() {
 
 let curTheme = "light";
 toggleBtn.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) return;
-  if (e.target.closest("svg")) {
-    if (curTheme === "light") {
-      darkIcon.classList.add("hidden");
-      lightIcon.classList.remove("hidden");
-      darkTheme();
-      curTheme = "dark";
-    } else {
-      darkIcon.classList.remove("hidden");
-      lightIcon.classList.add("hidden");
-      lightTheme();
-      curTheme = "light";
-    }
-  }
+if (e.target === e.currentTarget) return;
+if (e.target.closest("svg")) {
+if (curTheme === "light") {
+  darkIcon.classList.add("hidden");
+  lightIcon.classList.remove("hidden");
+  darkTheme();
+  curTheme = "dark";
+} else {
+  darkIcon.classList.remove("hidden");
+  lightIcon.classList.add("hidden");
+  lightTheme();
+  curTheme = "light";
+}
+}
 });
-
